84a90f8eba3de1669985c7d1486ae9d3978aeccd
nevtemu <artem.soroka@hotmail.com>
2021-01-26 12:10:53 +0400

    Added components structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './style.css';
 import {containers, social} from "./cards.mjs"
+import Main from './components/Main'
 
 function App() {
   return (
     <>
-    {containers[0].cards}
+      <Main/>
     </>
   );
 }
